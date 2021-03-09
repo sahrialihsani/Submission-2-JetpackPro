5c1977f21057ed5325da1853ee074774f543664d
Sahrial Ihsani Ishak <buildndrd@gmail.com>
2021-03-09 19:31:53 +0700

    Commit1

---
+++ app/src/main/java/com/sahrial/submission_2_jetpackpro/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sahrial.submission_2_jetpackpro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
